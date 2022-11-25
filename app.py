c0e7b6ab0ed164ba7d59d7dd976a6a61b68033ab
Pratiksha Rale <101017996+Pratiksha228@users.noreply.github.com>
2022-11-25 19:48:47 +0530

    .

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def uploadImage(key):
 
 def aboutMe():
     
-    st.markdown(requests.get(os.getenv('ABOUT_ME','https://github.com/Pratiksha228/Pratiksha228/main/README.md')).text, unsafe_allow_html=True)
+    st.markdown(requests.get(os.getenv('ABOUT_ME','https://raw.githubusercontent.com/Pratiksha228/Pratiksha228/main/README.md')).text, unsafe_allow_html=True)
 
 # Average Filter 
 
