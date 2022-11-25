cfa6903be057f0fb61d886718c4b15430235383c
Pratiksha Rale <101017996+Pratiksha228@users.noreply.github.com>
2022-11-25 19:45:14 +0530

    .

---
+++ demo.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.title("Hello Streamlit")
+
+st.header("Header")
+
+st.subheader("Sub header")
+
+st.text("keep growing and glowing as always")
+
+st.markdown(""" # h1 tag
+## h2 tag
+### h3 tag
+:moon:<br>
+:smile:
+:sunglasses:
+** bold **
+_ italics _
+""",True)
+
+st.latex
